059af1071757a6f0e41b6cfd42acecaf1496faf5
yWarsame <yasinhassan1013@gmail.com>
2026-01-30 11:45:37 +0100

    01/ Abstrakte Klasse Shape ohne vererbung erstmal gebaut

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,9 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        // Shape shape1 = new Shape(); // Fehler: Kann keine Instanz einer abstrakten Klasse erstellen
      
        
     }
+
+  
+
+   
 
 
     private static void output(String outputStr) {
@@ -15,4 +20,3 @@ public class App {
 
 
 }
-
